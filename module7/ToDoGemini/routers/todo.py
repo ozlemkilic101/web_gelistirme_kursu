f6318123241a5fd2bec7bd250848fa07ac401ca4
Ozlem Kilic <ozlemkilic101@gmail.com>
2026-03-24 00:53:20 +0300

    Yetkilendirme işlemleri yapıldı ve JWT kullanıldı.

---
+++ module7/ToDoGemini/routers/todo.py
@@ -0,0 +1,95 @@
+from fastapi import APIRouter, Depends, Path,status,HTTPException
+from models import ToDo
+from database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
+from routers.auth import get_current_user
+
+
+router = APIRouter(
+    prefix="/todo",
+    tags=["Todo"]
+)
+
+class TodoRequest(BaseModel):
+    title:str=Field(min_length=3)
+    description:str =Field(min_length=3,max_length=1000)
+    priority:int=Field(gt=0,lt=6)
+    complete:bool
+
+
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db  # yield return gibidir. teknik farkı yield kullanılan fonksiyonlar generatordur. Birden fazla şey döndürür.
+    finally:
+        db.close()
+
+db_dependency=Annotated[Session , Depends(get_db)]
+user_dependency=Annotated[dict,Depends(get_current_user)]
+
+@router.get("/")
+async def read_all(user:user_dependency,db:db_dependency):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    return db.query(ToDo).filter(ToDo.owner_id==user.get('id')).all()
+
+
+@router.get("/todo/{todo_id}",status_code=status.HTTP_200_OK)
+async def read_by_id(user:user_dependency,db: db_dependency,todo_id:int=Path(gt=0)):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    
+    todo=db.query(ToDo).filter(ToDo.id==todo_id).filter(ToDo.owner_id==user.get('id')).first()
+
+    if todo is not None:
+        return todo
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+
+
+@router.post("/todo",status_code=status.HTTP_201_CREATED)
+async def create_todo(user:user_dependency,db:db_dependency,todo_request:TodoRequest):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    todo=ToDo(**todo_request.dict(),owner_id=user.get('id'))
+    db.add(todo)
+    db.commit()
+
+
+@router.put("/todo/{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(user:user_dependency,db:db_dependency,todo_request:TodoRequest,todo_id:int=Path(gt=0)):
+
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    todo=db.query(ToDo).filter(ToDo.id==todo_id).filter(ToDo.owner_id==user.get('id')).first()
+
+    if todo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Todo not found")
+
+    todo.title=todo_request.title
+    todo.description=todo_request.description
+    todo.priority=todo_request.priority
+    todo.complete=todo_request.complete
+
+    db.add(todo)
+    db.commit()
+
+@router.delete("/todo/{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(user:user_dependency,db:db_dependency,todo_id:int=Path(gt=0)):
+
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    
+    todo=db.query(ToDo).filter(ToDo.id==todo_id).first()
+
+    if todo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Todo not found")
+
+   # db.query(ToDo).filter(ToDo.id==todo_id).delete()    alt satırdaki işlemle aynı ama delete riskli olduğu için id ' den yapıyorlar. ekstra bir önlem amaçlı.
+    db.delete(todo)
+    db.commit()
+
+
